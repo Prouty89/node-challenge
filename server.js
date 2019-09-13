1b7d9647a629125aaa73727d131cce3a60308976
Blake <bpro0821@gmail.com>
2019-09-13 17:05:41 -0500

    New MVP, changes completed to boolean rather than string

---
+++ server.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const helmet = require('helmet');
+
+const projectsRouter = require('./projects/projects-router.js');
+const resourcesRouter = require('./resources/resources-router.js');
+const tasksRouter = require('./tasks/tasks-router.js');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(logger);
+
+server.use('/projects', projectsRouter);
+server.use('/resources', resourcesRouter);
+server.use('/tasks', tasksRouter);
+
+function logger(req, res, next) {
+    const method = req.method;
+    const url = req.url;
+    const timestamp = Date.now();
+    console.log(`${method} request to '${url}' at ${timestamp}`);
+    next()
+};
+
+server.get('/', (req, res) => {
+    res.status(200).json({ Message: "it's Alive!" })
+});
+
+module.exports = server;
